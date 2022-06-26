733722bee2256c115cecdff47c9cc050055e9547
devopsseekerscloud <devops.seekerscloud@gmail.com>
2022-06-25 22:59:30 -0700

    3

--- src/main/java/lk/ijse/pos/POS/controller/CustomerController.java
+++ src/main/java/lk/ijse/pos/POS/controller/CustomerController.java
@@ -4,6 +4,8 @@ import lk.ijse.pos.POS.dto.request.CustomerRequestDto;
 import lk.ijse.pos.POS.service.CustomerService;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("api/v1/customer")
 public class CustomerController {
@@ -20,23 +22,25 @@ public class CustomerController {
     }
 
     @GetMapping
-    public String findCustomer() {
-        return "findCustomer";
+    public String findCustomer(@RequestParam String id) {
+        return customerService.findCustomer(id);
     }
 
     @PutMapping
-    public String updateCustomer() {
-        return "updateCustomer";
+    public String updateCustomer(
+            @RequestParam String id,
+            @RequestBody CustomerRequestDto dto) {
+        return customerService.updateCustomer(dto,id);
     }
 
     @DeleteMapping
-    public String deleteCustomer() {
-        return "deleteCustomer";
+    public String deleteCustomer(@RequestParam String id) {
+        return customerService.deleteCustomer(id);
     }
 
     @GetMapping(path = "/list")
-    public String listAllCustomers() {
-        return "listAllCustomers";
+    public List listAllCustomers() {
+        return customerService.listAllCustomers();
     }
 
 }
